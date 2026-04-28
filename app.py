471dd5135301e3cf19324b361a7235cfbc7812dc
Andrés Emmanuel Rueda Alor <teacherandresrueda@gmail.com>
2026-04-28 12:42:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+from auth import login, check_auth
+from modules import chispazo, melate
+
+st.set_page_config(page_title="MetaPattern Engine", layout="wide")
+
+login()
+check_auth()
 
 st.title("🎯 MetaPattern Engine PRO")
 
 tab1, tab2 = st.tabs(["Chispazo", "Melate"])
+
+with tab1:
+    chispazo.run()
+
+with tab2:
+    melate.run()
