66d4f1d067f947daa55171bff2bb126fa07ef373
Andrés Emmanuel Rueda Alor <teacherandresrueda@gmail.com>
2026-04-28 12:41:36 -0600

    Update analysis.py

--- utils/analysis.py
+++ utils/analysis.py
@@ -1,1 +1,8 @@
+def calcular_aciertos(nums_str, resultado_str):
+    if not resultado_str:
+        return 0
 
+    nums = set(map(int, nums_str.split(",")))
+    res = set(map(int, resultado_str.split(",")))
+
+    return len(nums & res)
